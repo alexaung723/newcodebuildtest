84008a88d3b22d77a5db55ad58ecfa4361f6d7ae
sameast <samuel.eastwood@slalom.com>
2022-07-25 13:12:40 -0600

    trying to get calculator enpoint to register

--- src/main/java/com/slalom/api/Calculator.java
+++ src/main/java/com/slalom/api/Calculator.java
@@ -1,2 +1,16 @@
-package com.slalom.api;public class Calculator {
+package com.slalom.api;
+
+
+
+public class Calculator {
+
+    long id;
+
+    int retval;
+
+    public Calculator(long id, int retval) {
+
+    }
+
+
 }
--- src/main/java/com/slalom/api/CalculatorController.java
+++ src/main/java/com/slalom/api/CalculatorController.java
@@ -1,2 +1,28 @@
-package com.slalom.api;public class CalculatorController {
+package com.slalom.api;
+
+
+import org.apache.tomcat.util.json.JSONParser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class CalculatorController {
+
+
+/*
+    @GetMapping("/calculator")
+    //test this via http://localhost:8080/calculator
+    public Greeting greeting(@RequestParam(value = "number1", defaultValue = "World") int x1 , @RequestParam
+            (value = "number2", defaultValue = "0") int x2) {
+       return "";
+    }
+
+
+ */
+
+    @GetMapping("/calculator")
+    //test this via http://localhost:8080/greeting and http://localhost:8080/greeting?name=Kesha
+    public int calculate(@RequestParam(value = "number1", defaultValue = "World") int x1 , @RequestParam
+            (value = "number2", defaultValue = "0") int x2) {
+        return x1 + x2;
+    }
 }
